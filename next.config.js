37675f4c06536902f358561dcd296feb4d9dd4b3
HiroyukiUrata <hilloween1031@gmail.com>
2023-04-12 11:45:54 +0900

    myfirst

--- next.config.js
+++ next.config.js
@@ -10,7 +10,7 @@ module.exports = {
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
           { key: "Access-Control-Allow-Origin", value: "*" },
-          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+          { key: "Access-Control-Allow-Methods", value: "*" },
           { key: "Access-Control-Allow-Headers", value: "authorization, content-type, x-client-info, apikey" },
         ]
       }
